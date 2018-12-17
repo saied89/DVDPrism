d91b5c366c0235c568032b11f52e442c765e0a5b
saied <saiedmomen@gmail.com>
2018-12-17 14:49:11 +0330

    fix shared transition fresco bug
    https://github.com/facebook/fresco/issues/1445

--- app/src/main/java/android/saied/com/filmcompass/ui/main/MainActivity.kt
+++ app/src/main/java/android/saied/com/filmcompass/ui/main/MainActivity.kt
@@ -3,11 +3,13 @@ package android.saied.com.filmcompass.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.saied.com.filmcompass.R
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import arrow.core.Try
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.facebook.drawee.view.SimpleDraweeView
+import androidx.core.app.ActivityCompat
+import androidx.core.app.SharedElementCallback
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +19,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        ActivityCompat.setExitSharedElementCallback(this, object : SharedElementCallback() {
+            override fun onSharedElementEnd(
+                sharedElementNames: List<String>,
+                sharedElements: List<View>,
+                sharedElementSnapshots: List<View>
+            ) {
+                super.onSharedElementEnd(sharedElementNames, sharedElements, sharedElementSnapshots)
+                for (view in sharedElements) {
+                    if (view is SimpleDraweeView) {
+                        view.drawable.setVisible(true, true)
+                    }
+                }
+            }
+        })
     }
 }
