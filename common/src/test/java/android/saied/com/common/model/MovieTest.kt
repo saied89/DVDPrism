9d7c0ef1ddac5621bb73cd17df003f200123d4bf
saied <saiedmomen@gmail.com>
2018-12-20 10:48:05 +0330

    fix movieTest

--- common/src/test/java/android/saied/com/common/model/MovieTest.kt
+++ common/src/test/java/android/saied/com/common/model/MovieTest.kt
@@ -9,7 +9,7 @@ internal class MovieTest {
     fun `posterUrl_250p is generated correctly`() {
         val sampleInput98pUrl = "https://static.metacritic.com/images/products/movies/0/f7fab2a23f79b99f37bdef9c2f8ea182-98.jpg"
         val sample250pUrl = "https://static.metacritic.com/images/products/movies/0/f7fab2a23f79b99f37bdef9c2f8ea182-250h.jpg"
-        val movie = Movie("", 0L, sampleInput98pUrl, 0)
+        val movie = Movie("", 0L, sampleInput98pUrl, 0, 0, "")
 
         assertEquals(sample250pUrl, movie.posterUrl_250p)
     }
