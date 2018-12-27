bdc7ee0bf919bd171ebb4d3fab70f0c9f0c3dfec
saied <saiedmomen@gmail.com>
2018-12-27 12:52:35 +0330

    make simple static json api

--- backend/src/main/java/android/saied/com/backend/Main.kt
+++ backend/src/main/java/android/saied/com/backend/Main.kt
@@ -1,20 +1,29 @@
 package android.saied.com.backend
 
+import android.saied.com.common.model.Movie
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.server.netty.EngineMain
 
-fun main() {
-    val server = embeddedServer(Netty, 8657) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
-            }
+fun main(args: Array<String>) = EngineMain.main(args)
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
         }
     }
-    server.start(true)
+
+    routing {
+        get("/") {
+            call.respond(Movie("saied", 0, "", 0, 0, ""))
+        }
+    }
 }
