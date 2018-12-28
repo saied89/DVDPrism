04ef6650362ca9a96162166cad734d73dec16457
saied <saiedmomen@gmail.com>
2018-12-28 09:56:35 +0330

    implement di

--- backend/src/main/java/android/saied/com/backend/Main.kt
+++ backend/src/main/java/android/saied/com/backend/Main.kt
@@ -1,29 +1,44 @@
 package android.saied.com.backend
 
+import android.saied.com.backend.di.dbModule
 import android.saied.com.common.model.Movie
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.netty.EngineMain
+import org.koin.ktor.ext.inject
+import org.koin.standalone.StandAloneContext.startKoin
 
-fun main(args: Array<String>) = EngineMain.main(args)
+fun main(args: Array<String>){
+    startKoin(listOf(dbModule))
+    EngineMain.main(args)
+}
 
 fun Application.module() {
     install(ContentNegotiation) {
+        install(StatusPages) {
+            exception<Throwable> {
+                call.respond(HttpStatusCode.InternalServerError, mapOf("error" to it.message))
+            }
+        }
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
         }
     }
 
+    val repository: MovieRepository by inject()
+
     routing {
         get("/") {
-            call.respond(Movie("saied", 0, "", 0, 0, ""))
+            call.respond(repository.getMovies())
         }
     }
 }
