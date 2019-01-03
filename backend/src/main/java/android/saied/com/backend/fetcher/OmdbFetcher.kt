88c6919cfa85f516da78bb164f2948b1c6085ba9
saied <saiedmomen@gmail.com>
2019-01-04 01:48:39 +0330

    implement omdb error handling

--- backend/src/main/java/android/saied/com/backend/fetcher/OmdbFetcher.kt
+++ backend/src/main/java/android/saied/com/backend/fetcher/OmdbFetcher.kt
@@ -1,22 +1,36 @@
 package android.saied.com.backend.fetcher
 
+import android.saied.com.backend.EnviromentPropertiesReader
 import android.saied.com.common.model.OmdbDetails
+import arrow.core.Try
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import io.ktor.client.HttpClient
+import io.ktor.client.call.ReceivePipelineException
 import io.ktor.client.request.get
 import io.ktor.http.URLProtocol
 
-class OmdbFetcher(private val client: HttpClient, private val omdbApiKey: String) {
+class OmdbFetcher(private val client: HttpClient, private val envReader: EnviromentPropertiesReader) {
 
     private val apiKey_queryLabel = "apikey"
     private val title_queryLabel = "t"
 
-    suspend fun getOmdbDetails(title: String): OmdbDetails = client.get {
-        this.url {
-            protocol = URLProtocol.HTTP
-            host = "www.omdbapi.com"
-            parameters.append(apiKey_queryLabel, omdbApiKey)
-            parameters.append(title_queryLabel, title)
+    suspend fun getOmdbDetails(title: String): Try<OmdbDetails> =
+        try {
+            val res: OmdbDetails = client.get {
+                this.url {
+                    protocol = URLProtocol.HTTP
+                    host = "www.omdbapi.com"
+                    parameters.append(apiKey_queryLabel, envReader.getOmdbApiKey())
+                    parameters.append(title_queryLabel, title)
+                }
+            }
+            Try.just(res)
+        } catch (exp: ReceivePipelineException) {
+            if(exp.cause is MissingKotlinParameterException)
+                Try.raise(OMDBMovieNotFoundException(title))
+            else
+                Try.raise(exp.cause)
         }
-    }
-
 }
+
+class OMDBMovieNotFoundException(title: String): Exception("$title was not found")
--- backend/src/test/kotlin/android/saied/com/backend/fetcher/OmdbFetcherKtTest.kt
+++ backend/src/test/kotlin/android/saied/com/backend/fetcher/OmdbFetcherKtTest.kt
@@ -1,13 +1,17 @@
 package android.saied.com.backend.fetcher
 
-import android.saied.com.common.model.OmdbDetails
+import android.saied.com.backend.EnviromentPropertiesReader
+import arrow.core.Failure
+import arrow.core.Success
+import arrow.core.Try
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.response
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.http.*
+import io.mockk.every
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -15,19 +19,19 @@ import kotlin.test.assertEquals
 internal class OmdbFetcherKtTest {
 
     @Test
-    fun `omdbfetcher sends correct request`() {
-        val content = with(javaClass.classLoader.getResource("OmdpSample.json")) {
-            readText()
-        }
+    fun `omdbfetcher returns data on OK response`() {
+        val content = javaClass.classLoader.getResource("OmdpSample.json").readText()
         val dummyApiKey = "dummyApiKey"
-
+        val mockEnvReader = mockk<EnviromentPropertiesReader>()
+        every { mockEnvReader.getOmdbApiKey() } returns dummyApiKey
         val testEngine = MockEngine {
             when (url.toString()) {
-                "http://www.omdbapi.com/?apikey=$dummyApiKey&t=cold+war" -> response(
-                    status = HttpStatusCode.OK,
-                    content = content,
-                    headers = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
-                )
+                "http://www.omdbapi.com/?apikey=$dummyApiKey&t=test" ->
+                    response(
+                        status = HttpStatusCode.OK,
+                        content = content,
+                        headers = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
+                    )
                 else -> {
                     error("Unhandled $url")
                 }
@@ -38,12 +42,39 @@ internal class OmdbFetcherKtTest {
                 serializer = JacksonSerializer()
             }
         }
-        val subject = OmdbFetcher(httpClient, dummyApiKey)
+
+        val subject = OmdbFetcher(httpClient, mockEnvReader)
 
         runBlocking {
-            val res = subject.getOmdbDetails("cold war")
-            assertEquals("1975", res.Year)
+            val res = subject.getOmdbDetails("test")
+            assert(res is Try.Success)
+            assertEquals("1975", (res as Success).value.Year)
         }
+    }
 
+    @Test
+    fun `omdbFetcher returns appropiate error when movie is not found`() {
+        val content = javaClass.classLoader.getResource("OmdbNotFound.json").readText()
+        val testEngine = MockEngine {
+            response(
+                content,
+                HttpStatusCode.OK,
+                headers = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
+            )
+        }
+        val httpClient = HttpClient(testEngine) {
+            install(JsonFeature) {
+                serializer = JacksonSerializer()
+            }
+        }
+        val mockEnvReader = mockk<EnviromentPropertiesReader>(relaxed = true)
+        val subject = OmdbFetcher(httpClient, mockEnvReader)
+
+        runBlocking {
+            val res = subject.getOmdbDetails("")
+
+            assert(res is Try.Failure)
+            assert((res as Failure).exception is OMDBMovieNotFoundException)
+        }
     }
 }
